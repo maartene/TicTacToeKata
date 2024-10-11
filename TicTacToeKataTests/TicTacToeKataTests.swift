31779e24bd4eb41dd90c055e698aa857b4e67751
Maarten Engels <maartene@thedreamweb.eu>
2024-10-11 08:37:32 +0200

    Pom01: test runs and fails as expected

--- TicTacToeKataTests/TicTacToeKataTests.swift
+++ TicTacToeKataTests/TicTacToeKataTests.swift
@@ -10,8 +10,8 @@ import Testing
 
 struct TicTacToeKataTests {
 
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    @Test("The test framework is behaving correctly") func example() async throws {
+        #expect(1 + 1 == 3)
     }
 
 }
