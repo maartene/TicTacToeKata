aa85100c29e595284c475a1b4babd4d1dd307303
Maarten Engels <maartene@thedreamweb.eu>
2024-10-11 08:57:28 +0200

    Pom01: end of pomodoro

--- NOTES.md
+++ NOTES.md
@@ -1,8 +1,13 @@
 # Notes
 
 ## Pomodoro 1
-### Goals:
-* Have a project scaffold setup
-* That can build and run a example single unit test pass to demonstrate that the project setup is working. (i.e. the unit test does not demonstrate any domain logic)
-* Demonstrate mockup version of UI
 
+### Goals:
+
+- Have a project scaffold setup ✅
+- That can build and run a example single unit test pass to demonstrate that the project setup is working. (i.e. the unit test does not demonstrate any domain logic) ✅
+- Demonstrate mockup version of UI ✅ (my main concern was how to make a grid for the play area in SwiftUI. Although it doesnt look like much yet, I feel confident that I now have a setup that I can improve on).
+
+### (Possible) goals for next pom:
+
+- Create a model that when its state changes, displays the changes in the UI
--- TicTacToeKata/ContentView.swift
+++ TicTacToeKata/ContentView.swift
@@ -7,13 +7,57 @@
 
 import SwiftUI
 
+struct CellView: View {
+    let value: Character
+    
+    var body: some View {
+        Text("\(value)")
+            .font(.largeTitle)
+            .frame(width: 30, height: 30)
+            .background(Color.gray.opacity(0.5))
+    }
+    
+    init(_ value: Character) {
+        self.value = value
+    }
+}
+
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("TicTacToe")
+                .font(.largeTitle)
+                .foregroundStyle(.secondary)
+            
+            Grid(horizontalSpacing: 5,
+                 verticalSpacing: 5) {
+                GridRow {
+                    CellView("X")
+                    CellView("O")
+                    CellView("X")
+                }
+                GridRow {
+                    CellView("O")
+                    CellView(" ")
+                    CellView("O")
+                }
+                GridRow {
+                    CellView("X")
+                    CellView("O")
+                    CellView("X")
+                }
+            }
+            .padding()
+            
+            Text("Player 1 wins")
+                .font(.title2)
+                .foregroundStyle(.primary)
+            
+            Button("Restart") {
+                print("Restart")
+            }
+            .buttonStyle(.borderedProminent)
+            
         }
         .padding()
     }
