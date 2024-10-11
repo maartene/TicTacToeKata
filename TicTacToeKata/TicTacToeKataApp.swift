bcc48df62152174e5c7529b55ead8a9ae3026959
Maarten Engels <maartene@thedreamweb.eu>
2024-10-11 08:31:13 +0200

    Initial Commit

---
+++ TicTacToeKata/TicTacToeKataApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeKataApp.swift
+//  TicTacToeKata
+//
+//  Created by Maarten Engels on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeKataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TicTacToeKataTests/TicTacToeKataTests.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeKataTests.swift
+//  TicTacToeKataTests
+//
+//  Created by Maarten Engels on 11/10/2024.
+//
+
+import Testing
+@testable import TicTacToeKata
+
+struct TicTacToeKataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
