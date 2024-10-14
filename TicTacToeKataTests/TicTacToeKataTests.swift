81360c68de7db020270d6dd6e1d006c7cc814422
Maarten Engels <maartene@thedreamweb.eu>
2024-10-14 08:31:40 +0200

    Pom02: Red: board is three wide

--- TicTacToeKataTests/TicTacToeKataTests.swift
+++ TicTacToeKataTests/TicTacToeKataTests.swift
@@ -8,9 +8,17 @@
 import Testing
 @testable import TicTacToeKata
 
-struct TicTacToeKataTests {
+struct TicTacToeBoard {
+    let width = 0
+}
 
+struct TicTacToeKataTests {
     @Test("The test framework is behaving correctly") func example() {
         #expect(1 + 1 == 2)
+    }
+    
+    @Test("A TicTacToe board is three wide") func ticTacToeBoardIsThreeWide() {
+        let board = TicTacToeBoard()
+        #expect(board.width == 3)
     }
 }
